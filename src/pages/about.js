5218721a6b5d89532deb32ba7c7c53b538496a6f
Arghz <dominique.gilleman@gmail.com>
2022-08-28 01:00:46 +0200

    basic with scss

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Page</h1>
+      <p>Dominique is a software, infra and ai Architect</p>
+    </Layout>
+  )
+}
+
+export default About
