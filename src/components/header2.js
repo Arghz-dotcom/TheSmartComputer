e12288823c9928c4f56338334d9c8c336dbe2eaf
Arghz <dominique.gilleman@gmail.com>
2022-09-01 22:57:05 +0200

    refactor header

--- src/components/header2.js
+++ src/components/header2.js
@@ -1,10 +1,8 @@
 import React from "react"
 
-const Header2 = () => {
+const Logo = () => {
     return (
-        <header class="bg-red-50">
-            <nav class="flex items-center justify-between p-6 container mx-auto">
-                <a href="#">
+        <a href="#">
                     <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 64">
                         <defs>
                             
@@ -26,33 +24,53 @@ const Header2 = () => {
                                         </g></g></g></g></g>
                     </svg>
                 </a>
-                <div class="text-lg text-gray-600 hidden lg:flex">
-                    <a href="#" class="block mt-4 lg:inline-block text-teal-600 lg:mt-0 mr-10">
-                        Home
-                    </a>
-                    <a href="#" class="block mt-4 lg:inline-block hover:text-gray-700 lg:mt-0 mr-10">
-                        Services
-                    </a>
-                    <a href="#" class="block mt-4 lg:inline-block hover:text-gray-700 lg:mt-0 mr-10">
-                        Portfolio
-                    </a>
-                    <a href="#" class="block hover:text-gray-700 mt-4 lg:inline-block lg:mt-0 mr-10">
-                        Company
-                    </a>
-                    <a href="#" class="block hover:text-gray-700 mt-4 lg:inline-block lg:mt-0">
-                        Contact
-                    </a>
-                </div>
-                <div class="flex items-center">
-                    <div class="block lg:hidden">
-                        <button class="flex items-center px-4 py-3 border rounded text-teal-500 border-teal-500 focus:outline-none">
-                            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <title>Menu</title>
-                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
+    )
+}
+
+const Menu = () => {
+    return (
+        <div class="text-lg text-gray-600 hidden lg:flex">
+            <a href="#" class="block mt-4 lg:inline-block text-teal-600 lg:mt-0 mr-10">
+                Home
+            </a>
+            <a href="#" class="block mt-4 lg:inline-block hover:text-gray-700 lg:mt-0 mr-10">
+                Services
+            </a>
+            <a href="#" class="block mt-4 lg:inline-block hover:text-gray-700 lg:mt-0 mr-10">
+                Portfolio
+            </a>
+            <a href="#" class="block hover:text-gray-700 mt-4 lg:inline-block lg:mt-0 mr-10">
+                Company
+            </a>
+            <a href="#" class="block hover:text-gray-700 mt-4 lg:inline-block lg:mt-0">
+                Contact
+            </a>
+        </div>
+    )
+}
+
+const Hamburger = () => {
+    return (
+        <div class="flex items-center">
+            <div class="block lg:hidden">
+                <button class="flex items-center px-4 py-3 border rounded text-teal-500 border-teal-500 focus:outline-none">
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+const Header2 = () => {
+    return (
+        <header class="bg-red-50">
+            <nav class="flex items-center justify-between p-6 container mx-auto">
+                <Logo />
+                <Menu />
+                <Hamburger />
             </nav>
         </header>
     )
