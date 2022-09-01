babcbbcd2dac238bfc0ee12b324d54db8e503c3d
Arghz <dominique.gilleman@gmail.com>
2022-09-01 14:37:53 +0200

    add chakra

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,27 @@ module.exports = {
   siteMetadata: {
     title: "Dominique Gilleman",
     description: "I teach JavaScript, React, GraphQL and Gatsby",
-    author: "Ibaslogic",
+    author: "DomDom",
   },
   plugins: [
-    "gatsby-plugin-sass",'gatsby-plugin-postcss',
+    "gatsby-plugin-sass",
+    'gatsby-plugin-postcss',
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
   ],
 }
